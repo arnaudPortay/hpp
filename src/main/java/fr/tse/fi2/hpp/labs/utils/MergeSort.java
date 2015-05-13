6be598a5169f89839ae4eaf9fddfe60f500d9244
Arnaud Portay <arnaud.portay@telecom-st-etienne.fr>
2015-05-13 16:29:42 +0200

    bla

--- src/main/java/fr/tse/fi2/hpp/labs/utils/MergeSort.java
+++ src/main/java/fr/tse/fi2/hpp/labs/utils/MergeSort.java
@@ -34,6 +34,30 @@ public class MergeSort {
 			int[] tempGauche=null;
 			int[] tempDroit=null;
 			
+			if (tab.length<20)
+			{
+				for(int i=1;i<tab.length;i++)
+	            {
+	            int memory=tab[i];
+	            int compt=i-1;
+	            boolean marqueur;
+	            do
+	                {
+	                marqueur=false;
+	                if (tab[compt]>memory)
+	                    {
+	                	tab[compt+1]=tab[compt];
+	                    compt--;
+	                    marqueur=true;
+	                    }
+	                if (compt<0) marqueur=false;
+	                }
+	            while(marqueur);
+	            tab[compt+1]=memory;
+	            }
+				
+		        return tab;
+			}
 			
 			if (tab.length % 2 == 0)
 			{
@@ -59,8 +83,7 @@ public class MergeSort {
 					}
 					tempDroit[j]=tab[(tab.length/2)+j];
 				}
-			}
-							
+			}			
 			
 			int[] resGauche = this.tri(tempGauche);
 			int[] resDroit = this.tri(tempDroit);
